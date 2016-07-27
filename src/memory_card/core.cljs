757239f21c8c9f1ccbbe757be1c01455df1d4976
Hunter Hutchinson <hunter.hutchinson@gmail.com>
2016-07-27 09:53:33 -0500

    basic playing-card-flipping

--- src/memory_card/core.cljs
+++ src/memory_card/core.cljs
@@ -1,20 +1,60 @@
 (ns memory-card.core
-  (:require [rum.core :as rum]))
+  (:require [rum.core :as rum]
+            [clojure.string :refer [replace capitalize]]
+            [goog.string :as gstring]))
+
+(defn suit [s]
+  (let [suits {:heart "&#9829;"
+               :diamond "&#9830;"
+               :spade "&#9824;"
+               :club "&#9827;"}]
+    (gstring/unescapeEntities (suits s))))
+
+(defn rankify [r]
+  (let [royalty #{:king :queen :jack :ace}]
+    (if (contains? royalty r)
+      (-> r name first capitalize)
+      (name r))))
+
+(defn kw->class [k]
+  (-> k name (replace #"-" "_")))
 
 (rum/defc label [text]
   [:div {:class "label"} text])
 
-(enable-console-print!)
+(rum/defc pcf-placement [l s]
+  [:span {:class ["suit" (kw->class l)]} (suit s)])
 
-(println "This text is printed from src/memory-card/core.cljs. Go ahead and edit it and see reloading in action....")
+(rum/defc playing-card-face [r s]
+  (pcf-placement :middle-center s))
+
+(rum/defc playing-card [r s]
+  [:div {:class ["card" (name s) (name r)]}
+   [:div {:class ["corner" "top"]}
+    [:span {:class "number"} (rankify r)]
+    [:span (suit s)]]
+   (playing-card-face r s)
+   [:div {:class ["corner" "bottom"]}
+    [:span {:class "number"} (rankify r)]
+    [:span (suit s)]]])
+
+(rum/defc flip-card []
+  [:div {:class "flip-container"}
+   [:div {:class "flipper"}
+    [:div {:class "front"}
+     [:div {:class ["card"]}]]
+    [:div {:class "back"}
+     (playing-card :queen :heart)]]])
+
+(enable-console-print!)
 
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "good Murk."}))
 
-
 (defn on-js-reload []
-  (rum/mount (label (:text @app-state)) (.getElementById js/document "app"))
+  (rum/mount (flip-card) (.getElementById js/document "app"))
+  
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
